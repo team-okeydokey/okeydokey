9521c7cf9d19d2cf38e87b3ba92ba914f3a649eb
Seongmin Park <seongminnpark@gmail.com>
2018-02-12 13:51:28 -0500

    Implement addressinitialization during migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,11 +5,102 @@ var Devices = artifacts.require("Devices");
 var Reservations = artifacts.require("Reservations");
 var Reviews = artifacts.require("Reviews");
 
-module.exports = function(deployer) {
-    deployer.deploy(OkeyDokeyGod);
-    deployer.deploy(OkeyDokey);
-    deployer.deploy(Houses);
-    deployer.deploy(Devices);
-    deployer.deploy(Reservations);
-    deployer.deploy(Reviews);
+/** Deploy dependancies first.
+ *
+ * Solution derived from 
+ * https://ethereum.stackexchange.com/questions/30572/truffle-post-deployment-actions
+ *
+ */
+module.exports = async function(deployer) {
+
+    let OkeyDokeyGodInst, OkeyDokeyInst;
+    let HousesInst, DevicesInst, ReservationsInst, ReviewsInst;
+
+    await Promise.all([
+        deployer.deploy(OkeyDokeyGod),
+        deployer.deploy(OkeyDokey),
+        deployer.deploy(Devices),
+        deployer.deploy(Reservations),
+        deployer.deploy(Reviews),
+        deployer.deploy(Houses)
+    ]).catch(function(error) {
+        console.log(error);
+    });
+
+    // Wait for deployment to finish.
+    instances = await Promise.all([
+        OkeyDokeyGod.deployed(),
+        OkeyDokey.deployed(),
+        Devices.deployed(),
+        Reservations.deployed(),
+        Reviews.deployed(),
+        Houses.deployed()
+    ]).catch(function(error) {
+        console.log(error);
+    });
+
+    // Assign instances.
+    OkeyDokeyGodInst = instances[0];
+    OkeyDokeyInst = instances[1];
+    DevicesInst = instances[2];
+    ReservationsInst = instances[3];
+    ReviewsInst = instances[4];
+    HousesInst = instances[5];
+
+    // Set addresses.
+    results = await Promise.all([
+        OkeyDokeyGodInst.updateAddress(OkeyDokeyInst.address),
+        OkeyDokeyInst.updateAddress(1, HousesInst.address),
+        OkeyDokeyInst.updateAddress(2, DevicesInst.address),
+        OkeyDokeyInst.updateAddress(3, ReservationsInst.address),
+        OkeyDokeyInst.updateAddress(4, ReviewsInst.address),
+        // HousesInst.initializeContracts(OkeyDokeyInst.address)
+        // DevicesInst.initializeContracts(OkeyDokeyInst.address),
+        // ReservationsInst.initializeContracts(OkeyDokeyInst.address),
+        // ReviewsInst.initializeContracts(OkeyDokeyInst.address)
+    ]).catch(function(error) {
+        console.log(error);
+    });
+
+
+    // Check initialization.
+    const godOkdk = await OkeyDokeyGodInst.getAddress();
+    // const okdkToken = await OkeyDokeyGodInst.getAddress(0);
+    const okdkHouses = await OkeyDokeyInst.getAddress(1);
+    const okdkDevices = await OkeyDokeyInst.getAddress(2);
+    const okdkReservations = await OkeyDokeyInst.getAddress(3);
+    const okdkReviews = await OkeyDokeyInst.getAddress(4);
+
+    var godCheck = (godOkdk == OkeyDokeyInst.address);
+    // boolean okeyDokeyCheck1 = (okdkToken == Token.address);
+    var okeyDokeyCheck2 = (okdkHouses == HousesInst.address);
+    var okeyDokeyCheck3 = (okdkDevices == DevicesInst.address);
+    var okeyDokeyCheck4 = (okdkReservations == ReservationsInst.address);
+    var okeyDokeyCheck5 = (okdkReviews == ReviewsInst.address);
+
+    printTestResult(godCheck, 'OkeyDokeyGod', 'OkeyDokey');
+    printTestResult(okeyDokeyCheck2, 'OkeyDokey', 'Houses');
+    printTestResult(okeyDokeyCheck3, 'OkeyDokey', 'Devices');
+    printTestResult(okeyDokeyCheck4, 'OkeyDokey', 'Reservations');
+    printTestResult(okeyDokeyCheck5, 'OkeyDokey', 'Reviews');
+
+    // Print addresses.
+    for (var inst of instances) {
+        printAddress(inst.constructor._json.contractName, 
+                     inst.address);
+    }
 };
+
+var printTestResult = function(success, parent, child) {
+    if (success) {
+        console.log('Initialization of ' + child + ' in ' + parent +' successful.');
+
+    } else {
+        console.log('Init error! Initialization of ' + child + ' in ' + parent + ' failed.');
+    }
+}
+
+var printAddress = function(name, address) {
+    console.log('Address of ' + name + ' is ' + address +'.');
+
+}

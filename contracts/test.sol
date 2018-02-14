a0ed72c8de88b5328f05c5d864e50e49de77d82d
beaverpark <pkh7802@gmail.com>
2018-02-14 01:36:30 -0500

    add test contract

---
+++ contracts/test.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.4.19;
+
+contract Test {
+ 
+    struct A {
+        bool active;
+        uint id; 
+    }
+    
+     A[] public array;
+    
+    function checkStruct(uint index) public {
+        // A memory a = array[index];
+         A storage a = array[index];
+        
+        require(a.active);
+    }
+    
+    function addToStruct(uint id) public {
+        A memory a = A(true, id);
+        array.push(a);
+    } 
+}

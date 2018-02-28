f0dad891340d31659c2f37524a9fe03cbf06ab7f
Seongmin Park <seongminnpark@gmail.com>
2018-02-28 05:21:54 -0500

    Add timing and cap limitations

--- contracts/OkeyDokeySale.sol
+++ contracts/OkeyDokeySale.sol
@@ -2,68 +2,29 @@ pragma solidity ^0.4.19;
 
 import "./SafeMath.sol";
 import "./Crowdsale.sol";
+import "./CappedTimedCrowdsale.sol";
 
 
 /**
  * @title TimedCrowdsale
  * @dev Crowdsale accepting contributions only within a time frame.
  */
-contract OkeyDokeySale is Crowdsale {
-  using SafeMath for uint256; 
+contract OkeyDokeySale is CappedTimedCrowdsale {
+    using SafeMath for uint256; 
 
-  uint256 public cap;
-  uint256 public openingTime;
-  uint256 public closingTime;
-
-  /**
-   * @dev Reverts if not in crowdsale time range. 
-   */
-  modifier onlyWhileOpen {
-    require(now >= openingTime && now <= closingTime);
-    _;
-  }
-
-  /**
-   * @dev Constructor, takes crowdsale opening and closing times.
-   * @param _cap Max amount of wei to be contributed
-   * @param _openingTime Crowdsale opening time
-   * @param _closingTime Crowdsale closing time
-   */
-  function OkeyDokeySale(uint256 _cap, uint256 _openingTime, uint256 _closingTime) public {
-    require(_cap > 0);
-    require(_openingTime >= now);
-    require(_closingTime >= _openingTime);
-
-    cap = _cap;
-    openingTime = _openingTime;
-    closingTime = _closingTime;
-  }
-
-  /**
-   * @dev Checks whether the period in which the crowdsale is open has already elapsed.
-   * @return Whether crowdsale period has elapsed
-   */
-  function hasClosed() public view returns (bool) {
-    return now > closingTime;
-  }
-
-  /**
-   * @dev Checks whether the cap has been reached. 
-   * @return Whether the cap was reached
-   */
-  function capReached() public view returns (bool) {
-    return weiRaised >= cap;
-  }
-
-  
-  /**
-   * @dev Extend parent behavior requiring to be within contributing period
-   * @param _beneficiary Token purchaser
-   * @param _weiAmount Amount of wei contributed
-   */
-  function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal onlyWhileOpen {
-    super._preValidatePurchase(_beneficiary, _weiAmount);
-    require(weiRaised.add(_weiAmount) <= cap);
-  }
+    /**
+     * @dev Constructor, takes all necessary arguments.
+     * @param _rate Number of token units a buyer gets per wei
+     * @param _wallet Address where collected funds will be forwarded to
+     * @param _token Address of the token being sold
+     * @param _cap Max amount of wei to be contributed
+     * @param _openingTime Crowdsale opening time
+     * @param _closingTime Crowdsale closing time
+     */
+    function OkeyDokeySale(uint256 _rate, address _wallet, OkeyDokeyToken _token, 
+                           uint256 _cap, uint256 _openingTime, uint256 _closingTime) 
+             CappedTimedCrowdsale(_cap, _openingTime, _closingTime)
+             Crowdsale(_rate, _wallet, _token) public {
+    }
 
 }

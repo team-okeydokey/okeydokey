3a66194c750cdab3b303753f1f60072009b2c507
Seongmin Park <seongminnpark@gmail.com>
2018-04-01 22:00:22 -0400

    Change default port

--- truffle.js
+++ truffle.js
@@ -2,7 +2,7 @@ module.exports = {
     networks: {
         development: {
             host: "localhost",
-            port: 8545,
+            port: 8546,
             network_id: "*", // Match any network id.
             from: "0x929FFF0071a12d66b9d2A90f8c3A6699551E91e3"
         }
